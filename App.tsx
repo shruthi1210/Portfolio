6db72878b384df7cb2ec30d7aa67b797889180c5
shruthi1210 <113176529+shruthi1210@users.noreply.github.com>
2025-03-18 16:03:56 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,38 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import NetflixTitle from './NetflixTitle';
-import ProfilePage from './profilePage/profilePage';
-import Browse from './browse/browse';
-import WorkPermit from './pages/WorkPermit';
-import WorkExperience from './pages/WorkExperience';
-import Recommendations from './pages/Recommendations';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import ContactMe from './pages/ContactMe';
-import Layout from './Layout';
-import Music from './pages/Music';
-import Reading from './pages/Reading';
-import Blogs from './pages/Blogs';
-import Certifications from './pages/Certifications';
+import React from "react";
+import "./styles/global.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<NetflixTitle />} />
-      <Route path="/browse" element={<Browse />} />
-      <Route path="/profile/:profileName" element={<Layout><ProfilePage /></Layout>} />
-      <Route path="/work-permit" element={<Layout><WorkPermit /></Layout>} />
-      <Route path="/work-experience" element={<Layout><WorkExperience /></Layout>} />
-      <Route path="/recommendations" element={<Layout><Recommendations /></Layout>} />
-      <Route path="/skills" element={<Layout><Skills /></Layout>} />
-      <Route path="/projects" element={<Layout><Projects /></Layout>} />
-      <Route path="/contact-me" element={<Layout><ContactMe /></Layout>} />
-      <Route path="/music" element={<Layout><Music /></Layout>} />
-      <Route path="/reading" element={<Layout><Reading /></Layout>} />
-      <Route path="/blogs" element={<Layout><Blogs /></Layout>} />
-      <Route path="/certifications" element={<Layout><Certifications /></Layout>} />
-    </Routes>
+    <div className="app-container">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+    </div>
   );
-};
+}
 
 export default App;
