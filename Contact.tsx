491d374ce27c5d6529eda6907e67328d80492e80
shruthi1210 <113176529+shruthi1210@users.noreply.github.com>
2025-03-18 16:18:30 -0400

    Update Contact.tsx

--- Contact.tsx
+++ Contact.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact</h2>
+      <p>Email: <a href="mailto:shruthiganesh1210@gmail.com">shruthiganesh1210@gmail.com</a></p>
+      <p>GitHub: <a href="https://github.com/shruthi1210">github.com/shruthi1210</a></p>
+      <p>LinkedIn: <a href="http://www.linkedin.com/in/shruthi-n-ganesh-4b6a3814b">LinkedIn Profile</a></p>
+    </section>
+  );
+};
+
+export default Contact;
 
